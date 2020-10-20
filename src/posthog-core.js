hanzoai/insights-js

commit b8aff7c74121640bd5b30ff85c7340d06b04811c

Extract method, add tests

src/posthog-core.js
@@ -94,7 +94,7 @@ var DOM_LOADED = false
  * PostHog Library Object
  * @constructor
  */
-var PostHogLib = function () {}
+export const PostHogLib = function () {}
 
 /**
  * create_mplib(token:string, config:object, name:string)
@@ -706,16 +706,7 @@ PostHogLib.prototype.capture = addOptOutCheckPostHogLib(function (event_name, pr
         return
     }
 
-    // set defaults
-    properties = properties || {}
-    properties['token'] = this.get_config('token')
-
-    // set $duration if time_event was previously called for this event
-    var start_timestamp = this['persistence'].remove_event_timer(event_name)
-    if (!_.isUndefined(start_timestamp)) {
-        var duration_in_ms = new Date().getTime() - start_timestamp
-        properties['$duration'] = parseFloat((duration_in_ms / 1000).toFixed(3))
-    }
+    const start_timestamp = this['persistence'].remove_event_timer(event_name)
 
     // update persistence
     this['persistence'].update_search_keyword(document.referrer)
@@ -727,30 +718,9 @@ PostHogLib.prototype.capture = addOptOutCheckPostHogLib(function (event_name, pr
         this['persistence'].update_referrer_info(document.referrer)
     }
 
-    // note: extend writes to the first object, so lets make sure we
-    // don't write to the persistence properties object and info
-    // properties object by passing in a new object
-
-    // update properties with pageview info and super-properties
-    properties = _.extend({}, _.info.properties(), this['persistence'].properties(), properties)
-
-    var property_blacklist = this.get_config('property_blacklist')
-    if (_.isArray(property_blacklist)) {
-        _.each(property_blacklist, function (blacklisted_prop) {
-            delete properties[blacklisted_prop]
-        })
-    } else {
-        console.error('Invalid value for property_blacklist config: ' + property_blacklist)
-    }
-
-    var sanitize_properties = this.get_config('sanitize_properties')
-    if (sanitize_properties) {
-        properties = sanitize_properties(properties, event_name)
-    }
-
     var data = {
         event: event_name,
-        properties: properties,
+        properties: this._calculate_event_properties(event_name, properties, start_timestamp),
     }
 
     var truncated_data = _.truncate(data, 255)
@@ -774,6 +744,41 @@ PostHogLib.prototype.capture = addOptOutCheckPostHogLib(function (event_name, pr
 
     return truncated_data
 })
+
+PostHogLib.prototype._calculate_event_properties = function (event_name, event_properties, start_timestamp) {
+    // set defaults
+    let properties = event_properties || {}
+    properties['token'] = this.get_config('token')
+
+    // set $duration if time_event was previously called for this event
+    if (!_.isUndefined(start_timestamp)) {
+        var duration_in_ms = new Date().getTime() - start_timestamp
+        properties['$duration'] = parseFloat((duration_in_ms / 1000).toFixed(3))
+    }
+
+    // note: extend writes to the first object, so lets make sure we
+    // don't write to the persistence properties object and info
+    // properties object by passing in a new object
+
+    // update properties with pageview info and super-properties
+    properties = _.extend({}, _.info.properties(), this['persistence'].properties(), properties)
+
+    var property_blacklist = this.get_config('property_blacklist')
+    if (_.isArray(property_blacklist)) {
+        _.each(property_blacklist, function (blacklisted_prop) {
+            delete properties[blacklisted_prop]
+        })
+    } else {
+        console.error('Invalid value for property_blacklist config: ' + property_blacklist)
+    }
+
+    var sanitize_properties = this.get_config('sanitize_properties')
+    if (sanitize_properties) {
+        properties = sanitize_properties(properties, event_name)
+    }
+
+    return properties
+}
 
 PostHogLib.prototype._create_map_key = function (group_key, group_id) {
     return group_key + '_' + JSON.stringify(group_id)

src/__tests__/posthog-core.js
@@ -1,0 +1,54 @@
+import { PostHogLib } from '../posthog-core'
+import { _ } from '../utils'
+
+describe('_calculate_event_properties()', () => {
+    given('subject', () =>
+        given.posthog._calculate_event_properties(given.event_name, given.properties, given.start_timestamp)
+    )
+
+    given('event_name', () => 'custom_event')
+    given('properties', () => ({ event: 'prop' }))
+
+    given('posthog', () => new PostHogLib())
+    given('config', () => ({
+        token: 'testtoken',
+        property_blacklist: given.property_blacklist,
+        sanitize_properties: given.sanitize_properties,
+    }))
+
+    beforeEach(() => {
+        jest.spyOn(_.info, 'properties').mockReturnValue({ $lib: 'web' })
+
+        given.posthog.get_config = (key) => given.config[key]
+
+        given.posthog.persistence = {}
+        given.posthog.persistence.properties = () => ({ persistent: 'prop' })
+    })
+
+    it('returns calculated properties', () => {
+        expect(given.subject).toEqual({
+            token: 'testtoken',
+            event: 'prop',
+            $lib: 'web',
+            persistent: 'prop',
+        })
+    })
+
+    it('respects property_blacklist', () => {
+        given('property_blacklist', () => ['$lib', 'persistent'])
+
+        expect(given.subject).toEqual({
+            token: 'testtoken',
+            event: 'prop',
+        })
+    })
+
+    it('calls sanitize_properties', () => {
+        given('sanitize_properties', () => (props, event_name) => ({ token: props.token, event_name }))
+
+        expect(given.subject).toEqual({
+            event_name: given.event_name,
+            token: 'testtoken',
+        })
+    })
+})

commit 554e908f4ed5e2e1e51bea6b07a337811dea829d

Always send distinct_id to server to avoid 400 status (#95)
This was broken in commit b8aff7c74121640bd5b30ff85c7340d06b04811c

src/posthog-core.js
@@ -751,6 +751,8 @@ PostHogLib.prototype._calculate_event_properties = function (event_name, event_p
     properties['token'] = this.get_config('token')
 
     if (event_name === '$snapshot') {
+        const persistenceProps = this.persistence.properties()
+        properties['distinct_id'] = persistenceProps.distinct_id
         return properties
     }
 

src/__tests__/posthog-core.js
@@ -22,7 +22,7 @@ describe('_calculate_event_properties()', () => {
         given.posthog.get_config = (key) => given.config[key]
 
         given.posthog.persistence = {}
-        given.posthog.persistence.properties = () => ({ persistent: 'prop' })
+        given.posthog.persistence.properties = () => ({ distinct_id: 'abc', persistent: 'prop' })
     })
 
     it('returns calculated properties', () => {
@@ -30,6 +30,7 @@ describe('_calculate_event_properties()', () => {
             token: 'testtoken',
             event: 'prop',
             $lib: 'web',
+            distinct_id: 'abc',
             persistent: 'prop',
         })
     })
@@ -40,6 +41,7 @@ describe('_calculate_event_properties()', () => {
         expect(given.subject).toEqual({
             token: 'testtoken',
             event: 'prop',
+            distinct_id: 'abc',
         })
     })
 
@@ -59,6 +61,7 @@ describe('_calculate_event_properties()', () => {
         expect(given.subject).toEqual({
             token: 'testtoken',
             $snapshot_data: {},
+            distinct_id: 'abc',
         })
     })
 })